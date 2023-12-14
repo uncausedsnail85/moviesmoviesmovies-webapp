af27ab3168e6c77300295cd9bfb9c7848612a693
Bryan Ang <bryan.ang>
2023-12-14 22:17:32 +0800

    finish user profile

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import * as client from "../client/userClient";
 import "./index.css";
 
 // shows the profile page of a particular user
@@ -10,48 +11,68 @@ function Profile() {
 
     const pageUsername = useParams().username;
     const loggedInUser = useSelector((state) => state.userReducer).user; // get logged in state
-    // const [user, setUser] = useState()
+    const [pageUser, setPageUser] = useState(null);
+    const [pageOwnerFlag, setPageOwnerFlag] = useState(false)
+
+    const getPageUser = async () => {
+        if (pageUsername == loggedInUser.username) {
+            setPageUser(loggedInUser);
+            setPageOwnerFlag(true);
+        } else {
+            const results = await client.findUserByUsername(pageUsername);
+            setPageUser(results);
+            setPageOwnerFlag(false);
+        }
+    }
+
+    useEffect(() => {
+        getPageUser();
+    }, [pageUsername])
 
     return (
         <>
             <h2>Profile</h2>
+            {pageUser && <>
+                <div class="row align-items-center m3-profile-row">
+                    <div class="col-2 m3-profile-col1">
+                        Username:</div>
+                    <div class="col-10 col-sm-7 col-xl-6" >
+                        {pageUser.username}</div>
+                </div>
+                <div class="row align-items-center m3-profile-row">
+                    <div class="col-2 m3-profile-col1">
+                        First name:</div>
+                    <div class="col-10 col-sm-7 col-xl-6" >
+                        {pageUser.firstName}</div>
+                </div>
+                <div class="row align-items-center m3-profile-row">
+                    <div class="col-2 m3-profile-col1">
+                        Last name:</div>
+                    <div class="col-10 col-sm-7 col-xl-6" >
+                        {pageUser.lastName}</div>
+                </div>
+            </>}
+
+            {pageUser && pageOwnerFlag &&
+                <div class="row align-items-center m3-profile-row">
+                    <div class="col-2 m3-profile-col1">
+                        Email:</div>
+                    <div class="col-10 col-sm-7 col-xl-6" >
+                        {pageUser.email}</div>
+                </div>}
+
             {loggedInUser != "" && loggedInUser.username === pageUsername && (
-                <div>
+                <div className="p-3">
                     <button
-                        className="btn btn-info" type="submit"
+                        className="btn btn-info ms-3" type="submit"
                         onClick={() => navigate(`/profile/settings`)}>
                         Edit Profile</button>
                     <button
-                        className="btn btn-warning" type="submit"
+                        className="btn btn-warning ms-3" type="submit"
                         onClick={() => navigate(`/signout`)}>
                         Sign Out</button>
                 </div>)}
-            <div class="row align-items-center m3-profile-row">
-                <div class="col-2 m3-profile-col1">
-                    Username:</div>
-                <div class="col-10 col-sm-7 col-xl-6" >
-                    {pageUsername}</div>
-            </div>
-            <div class="row align-items-center m3-profile-row">
-                <div class="col-2 m3-profile-col1">
-                    First name:</div>
-                <div class="col-10 col-sm-7 col-xl-6" >
-                    TODO: DB</div>
-            </div>
-            <div class="row align-items-center m3-profile-row">
-                <div class="col-2 m3-profile-col1">
-                    Last name:</div>
-                <div class="col-10 col-sm-7 col-xl-6" >
-                    TODO: DB</div>
-            </div>
-            <div class="row align-items-center m3-profile-row">
-                <div class="col-2 m3-profile-col1">
-                    Email:</div>
-                <div class="col-10 col-sm-7 col-xl-6" >
-                    TODO: DB</div>
-            </div>
 
-            <h2>Discussions</h2>
             <h2>Likes</h2>
 
             {/* <>JSON ccurrent logged in user:</>
